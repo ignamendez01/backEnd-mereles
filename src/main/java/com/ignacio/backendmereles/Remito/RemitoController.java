b443f8e8c1d2cd88edc5cc70d0e0377014a51690
Ignacio Mendez <ignacio.mendez@ing.austral.edu.ar>
2025-03-14 17:50:57 -0300

    back to basics

--- src/main/java/com/ignacio/backendmereles/Remito/RemitoController.java
+++ src/main/java/com/ignacio/backendmereles/Remito/RemitoController.java
@@ -1,7 +1,6 @@
 package com.ignacio.backendmereles.Remito;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -11,8 +10,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Sinks;
 
 import java.util.List;
 import java.util.Optional;
@@ -23,78 +20,60 @@ import java.util.Optional;
 public class RemitoController {
 
     private final RemitoService remitoService;
-    private final Sinks.Many<List<Remito>> sink;
 
     public RemitoController(RemitoService remitoService) {
         this.remitoService = remitoService;
-        this.sink = Sinks.many().replay().latest();
     }
 
     @GetMapping("/activos")
-    public Flux<ServerSentEvent<List<Remito>>> obtenerRemitosActivos() {
-        return sink.asFlux()
-                .map(remitosActivos -> ServerSentEvent.builder(remitosActivos).build())
-                .startWith(ServerSentEvent.builder(remitoService.obtenerRemitosActivos()).build());
+    public ResponseEntity<List<Remito>> obtenerRemitosActivos() {
+        return ResponseEntity.ok(remitoService.obtenerRemitosActivos());
     }
 
     @GetMapping("/locales")
-    public Flux<ServerSentEvent<List<Remito>>> obtenerRemitosLocales() {
-        return sink.asFlux()
-                .map(remitosActivos -> ServerSentEvent.builder(remitosActivos).build())
-                .startWith(ServerSentEvent.builder(remitoService.obtenerRemitosActivosNoEnviados()).build());
+    public ResponseEntity<List<Remito>> obtenerRemitosLocales() {
+        return ResponseEntity.ok(remitoService.obtenerRemitosActivosNoEnviados());
     }
 
     @PostMapping("/generar")
     public ResponseEntity<Remito> generarRemito(@RequestBody Remito remito) {
         Remito nuevoRemito = remitoService.crearRemito(remito);
-        sink.tryEmitNext(remitoService.obtenerRemitosActivos());
-        sink.tryEmitNext(remitoService.obtenerRemitosActivosNoEnviados());
         return ResponseEntity.ok(nuevoRemito);
     }
 
     @PostMapping("/enviar")
     public ResponseEntity<Remito> enviarRemito(@RequestBody Remito remito) {
         Remito nuevoRemito = remitoService.enviarRemito(remito);
-        sink.tryEmitNext(remitoService.obtenerRemitosActivos());
-        sink.tryEmitNext(remitoService.obtenerRemitosActivosNoEnviados());
         return ResponseEntity.ok(nuevoRemito);
     }
 
     @PatchMapping("/{id}/desactivar")
     public ResponseEntity<Remito> desactivarRemito(@PathVariable Long id) {
         Optional<Remito> remitoDesactivado = remitoService.desactivarRemito(id);
-        sink.tryEmitNext(remitoService.obtenerRemitosActivos());
-        sink.tryEmitNext(remitoService.obtenerRemitosActivosNoEnviados());
         return remitoDesactivado.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @PatchMapping("/{id}/enviar")
     public ResponseEntity<Remito> enviarRemitoId(@PathVariable Long id) {
         Optional<Remito> remitoEnviado = remitoService.enviarRemito(id);
-        sink.tryEmitNext(remitoService.obtenerRemitosActivos());
-        sink.tryEmitNext(remitoService.obtenerRemitosActivosNoEnviados());
         return remitoEnviado.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @PatchMapping("/{id}/pesar")
     public ResponseEntity<Remito> pesarRemitoId(@PathVariable Long id) {
         Optional<Remito> remitoEnviado = remitoService.pesarRemito(id);
-        sink.tryEmitNext(remitoService.obtenerRemitosActivos());
         return remitoEnviado.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @PatchMapping("/{id}/egresar")
     public ResponseEntity<Remito> egresarRemitoId(@PathVariable Long id) {
         Optional<Remito> remitoEnviado = remitoService.egresarRemito(id);
-        sink.tryEmitNext(remitoService.obtenerRemitosActivos());
         return remitoEnviado.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @PutMapping("/{id}/actualizar")
     public ResponseEntity<Remito> actualizarRemito(@PathVariable Long id, @RequestBody RemitoUpdateRequest remitoUpdateRequest) {
         Optional<Remito> remitoActualizado = remitoService.actualizarRemito(id, remitoUpdateRequest);
-        sink.tryEmitNext(remitoService.obtenerRemitosActivos());
-        sink.tryEmitNext(remitoService.obtenerRemitosActivosNoEnviados());
         return remitoActualizado.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
--- src/main/java/com/ignacio/backendmereles/Tacho/TachoController.java
+++ src/main/java/com/ignacio/backendmereles/Tacho/TachoController.java
@@ -3,11 +3,8 @@ package com.ignacio.backendmereles.Tacho;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Sinks;
 
 import java.io.IOException;
 import java.util.List;
@@ -19,18 +16,9 @@ import java.util.Optional;
 public class TachoController {
 
     private final TachoService tachoService;
-    private final Sinks.Many<List<Tacho>> sink;
 
     public TachoController(TachoService tachoService) {
         this.tachoService = tachoService;
-        this.sink = Sinks.many().replay().latest();
-    }
-
-    @GetMapping("/stream")
-    public Flux<ServerSentEvent<List<Tacho>>> streamTachos() {
-        return sink.asFlux()
-                .map(tachos -> ServerSentEvent.builder(tachos).build())
-                .startWith(ServerSentEvent.builder(tachoService.obtenerTachosActivos()).build());
     }
 
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
@@ -40,7 +28,6 @@ public class TachoController {
             @RequestParam("imagen") MultipartFile imagen) {
         try {
             Tacho nuevoTacho = tachoService.crearTacho(descripcion, peso, imagen);
-            sink.tryEmitNext(tachoService.obtenerTachosActivos());
             return ResponseEntity.ok(nuevoTacho);
         } catch (IOException e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
@@ -64,16 +51,14 @@ public class TachoController {
 
     @PutMapping("/{id}")
     public ResponseEntity<Tacho> actualizarTacho(@PathVariable Long id, @RequestParam("descripcion") String descripcion,
-                                                   @RequestParam("peso") Double peso, @RequestParam(value = "imagen", required = false) MultipartFile imagen) throws IOException {
+                                                 @RequestParam("peso") Double peso, @RequestParam(value = "imagen", required = false) MultipartFile imagen) throws IOException {
         Optional<Tacho> tachoActualizado = tachoService.actualizarTacho(id, descripcion, peso, imagen);
-        sink.tryEmitNext(tachoService.obtenerTachosActivos());
         return tachoActualizado.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @PatchMapping("/{id}/desactivar")
     public ResponseEntity<Tacho> desactivarTacho(@PathVariable Long id) {
         Optional<Tacho> tachoDesactivado = tachoService.desactivarTacho(id);
-        sink.tryEmitNext(tachoService.obtenerTachosActivos());
         return tachoDesactivado.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
